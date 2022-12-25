ae1368beef53125f83b7a78267bab705b16dcc66
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-25 14:03:01 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,19 +3,19 @@ from os import environ
 from os import getenv
 
 class Config(object):
-	APP_ID = int(14055090)
-	API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
-	BOT_TOKEN = "5835513450:AAFI1bXJnB9YmZqnrCtUEKc7u4qQ8LSE-Mo"
-	SESSION = "BQDWdrIAYYrWyKYCB60R99bEzuASKnx6JuO7aRM9Z6O9wEYNZ7-ETLlZV8C7kWGH0-qOTq3_Vhb8ZezG-gMEx_d8XdAZvruasqsv9pdc7FNmWuhOeZ2tAvVRyUERakfxKgfrPYAQQDbsLnh3-HhILawL6TrV-S_W8lddqPZ1k_Fhe3OHuwMbsb30YkSr4dQIh3EpK2MNopJN6FYkAWUasMYgf-ofG8zQ4f8mj8NkM1A4zY2tNtEOeUk-9qnsNVs6_VzMDBOhKhYZqw96l-Gs-xxs4UeqRCnmmZBmdXjnui7pwZIN5yvQFRS3OUKn9Yb3uXkhA3VRay9M-kU3lZesNFRMou2lWgAAAAE0PH9pAA"
-	BOT_USERNAME = "ImRishmika_Bot"
+	APP_ID = int(190)
+	API_HASH = "a46f7b439a69fc450f754e9"
+	BOT_TOKEN = "5835513450:AB9YmZqnrCtUEKc7u4qQ8LSE-Mo"
+	SESSION = "BQDWdrIAYYrWyKYCB60R99bEzuASKnx6JuO8XdAZvruasqsv9pdc7FNmWuhOeZ2tAvVRyUERakfxKgfrPYAQQDbsLnh3-HhILawL6TrV-S_W8lddqPZ1k_Fhe3OHuwMbsb30YkSr4dQIh3EpK2MNopJN6FYkAWUasMYgf-ofG8zQ4f8mj8NkM1A4zY2tNtEOeUk-9qnsNVs6_VzMDBOhKhYZqw96l-Gs-xxs4UeqRCnmmZBmdXjnui7pwZIN5yvQFRS3OUKn9Yb3uXkhA3VRay9M-kU3lZesNFRMou2lWgAAAAE0PH9pAA"
+	BOT_USERNAME = "ImRishmBot"
 	BOT_NAME = "ʀιѕнмιкα ѕαnᴅαnυ Aѕѕιѕƚαиƚ"
-	OWNER = int(5171347305)
-	LOG_GRP = int(-1001551504918)
-	START_STICKER = "CAACAgIAAx0CZIiVngABCGUzY6gD_MMWGMwtoCenWWywZCPObeMAAgoeAAIkFMFJvDGzJJGdXnksBA"
+	OWNER = int(517134305)
+	LOG_GRP = int(-1001504918)
+	START_STICKER = "CAACAgIAAx0CZIiVngABCGUzY6gD_MMWGCPObeMAAgoeAAIkFMFJvDGzJJGdXnksBA"
 	CHANNEL = "EmoBotDevolopers"
 	CHANNEL_ID = int(-1001719968457)
 	GROUP = "EmoBotSupport"
 	START_PIC = "https://telegra.ph/file/1338f604c103fd8d35f66.jpg"
 	C_CHANNEL = "AboutRishmika"
-	MONGODB_URI = "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-	ARQ_API_KEY = "TLKZEM-FSYGYW-OTINGB-RIMVDM-ARQ"
+	MONGODB_URI = "mongodb+srv://ongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+	ARQ_API_KEY = "TLKZEM-FSNGB-RIMVDM-ARQ"
