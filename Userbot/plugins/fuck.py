f8e5b4c9820e138dbdda7e553cb5522d6dd3ab80
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-25 13:24:00 +0530

    Create fuck.py

---
+++ Userbot/plugins/fuck.py
@@ -0,0 +1,35 @@
+from Userbot import bot
+from pyrogram import filters, Client
+from pyrogram.types import Message
+import asyncio
+
+@bot.on_message(filters.command('fuck'))
+async def command1(bot, message):
+  message=await message.reply_text("Bot :- I Wan,t Sex ")
+  #asyncio.sleep(2)
+  #asyncio.sleep(2)
+  await message.edit("Bot Is Fucking Now... \n`Finding  Sexy Girl`\nETA: 0m, 12s")
+  #asyncio.sleep(2)
+  await message.edit("Bot Finded Sexy Girl \n Bot Try To Chat With Her")
+  #asyncio.sleep(2)
+  await message.edit("Bot :- Can You Sex With Me")
+  #asyncio.sleep(2)
+  await message.edit("Girl :- Ok Sexy Boy Wait I Wan't Remove My Cloths")
+  #asyncio.sleep(2)
+  await message.edit("Bot :- Ok Sexy Girl")
+  #asyncio.sleep(2)
+  await message.edit("Girl :- Kiss Me , ah ah ah")
+  #asyncio.sleep(2)
+  await message.edit("Bot :- Your Boobs is Very Cute")
+  #asyncio.sleep(2)
+  await message.edit("Girl :- Sex Me Please I Have අසහනය.. ")
+  #asyncio.sleep(2)
+  await message.edit("Girl :- ah, ah, ah, Sexy , wow Asome ")
+  #asyncio.sleep(2)
+  await message.edit("Girl :- ah, ah, ah, Sexy , wow Asome ")
+  #asyncio.sleep(2)
+  await message.edit("Bot :- i Record Your Nudes I Upload In Youtube Wait")
+  #asyncio.sleep(2)
+  await message.edit("Bot :- I Send My Users Wait..")
+  #asyncio.sleep(2)
+  await message.edit("Sexy Youtube link :- https://www.youtube.com/watch?v=_Oepy4QFNQs")
